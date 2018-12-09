02529f204da23975ddbdb368e30a9b490799bc08
jeenoo <jeenoolim@gmail.com>
2018-12-09 23:23:54 +0900

    jenkins test

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cluster = require('cluster');
+var os = require('os');
+var uuid = require('uuid');
+const port = 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//키생성 - 서버 확인용
+const instance_id = uuid.v4();
 
-var app = express();
+/**
+ * 워커 생성
+ */
+const cpuCount = os.cpus().length; //CPU 수
+const workerCount = cpuCount / 2;
+//2개의 컨테이너에 돌릴 예정 CPU수 / 2
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//마스터일 경우
+if (cluster.isMaster) {
+	console.log('서버 ID : ' + instance_id);
+	console.log('서버 CPU 수 : ' + cpuCount);
+	console.log('생성할 워커 수 : ' + workerCount);
+	console.log(workerCount + '개의 워커가 생성됩니다\n');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+	//워커 메시지 리스너
+	var workerMsgListener = function(msg) {
+		var worker_id = msg.worker_id;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+		//마스터 아이디 요청
+		if (msg.cmd === 'MASTER_ID') {
+			cluster.workers[worker_id].send({ cmd: 'MASTER_ID', master_id: instance_id });
+		}
+	};
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+	//CPU 수 만큼 워커 생성
+	for (var i = 0; i < workerCount; i++) {
+		console.log('워커 생성 [' + (i + 1) + '/' + workerCount + ']');
+		var worker = cluster.fork();
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+		//워커의 요청메시지 리스너
+		worker.on('message', workerMsgListener);
+	}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+	//워커가 online상태가 되었을때
+	cluster.on('online', function(worker) {
+		console.log('워커 온라인 - 워커 ID : [' + worker.process.pid + ']');
+	});
+
+	//워커가 죽었을 경우 다시 살림
+	cluster.on('exit', function(worker) {
+		console.log('워커 사망 - 사망한 워커 ID : [' + worker.process.pid + ']');
+		console.log('다른 워커를 생성합니다.');
+
+		var worker = cluster.fork();
+		//워커의 요청메시지 리스너
+		worker.on('message', workerMsgListener);
+	});
+
+	//워커일 경우
+} else if (cluster.isWorker) {
+	var express = require('express');
+	var app = express();
+	var worker_id = cluster.worker.id;
+	var master_id;
+
+	var server = app.listen(port, function() {
+		console.log('Express 서버가 ' + server.address().port + '번 포트에서 Listen중입니다.');
+	});
+
+	//마스터에게 master_id 요청
+	process.send({ worker_id: worker_id, cmd: 'MASTER_ID' });
+	process.on('message', function(msg) {
+		if (msg.cmd === 'MASTER_ID') {
+			master_id = msg.master_id;
+		}
+	});
+
+	app.get('/', function(req, res) {
+		res.send('안녕하세요 저는<br>[' + master_id + ']서버의<br>워커 [' + cluster.worker.id + '] 입니다.');
+	});
+	//워커 킬링 테스트
+	app.get('/kill', function(req, res) {
+		cluster.worker.kill();
+		res.send('워커킬러 호출됨');
+	});
+}
 
 module.exports = app;
